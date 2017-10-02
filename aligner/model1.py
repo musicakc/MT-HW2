2049dae36601dc6b189be20b5b5a64ef456b03a1
Amitoj Kaur Chawla <amitoj1606@gmail.com>
2017-10-02 19:54:25 -0400

    understanding data

---
+++ aligner/model1.py
@@ -0,0 +1,58 @@
+'''
+IBM Model 1 implementation for Machine Translation class HW2 Aligner
+'''
+
+#!/usr/bin/env python
+import optparse
+import sys
+from collections import defaultdict
+
+optparser = optparse.OptionParser()
+optparser.add_option("-d", "--data", dest="train", default="data/hansards", help="Data filename prefix (default=data)")
+optparser.add_option("-e", "--english", dest="english", default="e", help="Suffix of English filename (default=e)")
+optparser.add_option("-f", "--french", dest="french", default="f", help="Suffix of French filename (default=f)")
+optparser.add_option("-t", "--threshold", dest="threshold", default=0.5, type="float", help="Threshold for aligning with Dice's coefficient (default=0.5)")
+optparser.add_option("-n", "--num_sentences", dest="num_sents", default=sys.maxint, type="int", help="Number of sentences to use for training and alignment")
+(opts, _) = optparser.parse_args()
+f_data = "%s.%s" % (opts.train, opts.french)
+e_data = "%s.%s" % (opts.train, opts.english)
+
+sys.stderr.write("Training with IBM Model 1...")
+bitext = [[sentence.strip().split() for sentence in pair] for pair in zip(open(f_data), open(e_data))[:opts.num_sents]]
+
+#Count of french words
+f_count = defaultdict(float)
+#Count of english words
+e_count = defaultdict(float)
+
+fe_count = defaultdict(float)
+f_vocab = set()
+e_vocab = set()
+
+print bitext
+
+'''
+for (n, (f, e)) in enumerate(bitext):
+  for f_i in set(f):
+    f_count[f_i] += 1
+    for e_j in set(e):
+      fe_count[(f_i,e_j)] += 1
+  for e_j in set(e):
+    e_count[e_j] += 1
+  if n % 500 == 0:
+    sys.stderr.write(".")
+
+dice = defaultdict(int)
+for (k, (f_i, e_j)) in enumerate(fe_count.keys()):
+  dice[(f_i,e_j)] = 2.0 * fe_count[(f_i, e_j)] / (f_count[f_i] + e_count[e_j])
+  if k % 5000 == 0:
+    sys.stderr.write(".")
+sys.stderr.write("\n")
+
+for (f, e) in bitext:
+  for (i, f_i) in enumerate(f): 
+    for (j, e_j) in enumerate(e):
+      if dice[(f_i,e_j)] >= opts.threshold:
+        sys.stdout.write("%i-%i " % (i,j))
+  sys.stdout.write("\n")
+'''
