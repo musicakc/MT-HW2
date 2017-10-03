b117d736d5f84d491b2632e3f2a18566ba73de11
Amitoj Kaur Chawla <amitoj1606@gmail.com>
2017-10-02 20:54:44 -0400

    calculated theta probabilities

--- aligner/model1.py
+++ aligner/model1.py
@@ -18,6 +18,7 @@ f_data = "%s.%s" % (opts.train, opts.french)
 e_data = "%s.%s" % (opts.train, opts.english)
 
 sys.stderr.write("Training with IBM Model 1...")
+
 bitext = [[sentence.strip().split() for sentence in pair] for pair in zip(open(f_data), open(e_data))[:opts.num_sents]]
 
 #Count of french words
@@ -26,10 +27,72 @@ f_count = defaultdict(float)
 e_count = defaultdict(float)
 
 fe_count = defaultdict(float)
+sum_t = defaultdict(float)
+
+#Theta probability
+t_prob = defaultdict(float)
+#Probability of french words
+f_prob = defaultdict(float)
+#Probability of english words
+e_prob = defaultdict(float)
+
+#Update vocabularies from bitext
 f_vocab = set()
 e_vocab = set()
+for i in range(opts.num_sents):
+  sen = [sen for sen in bitext[i]]
+  f_vocab.update(set(sen[0]))
+  e_vocab.update(set(sen[1]))
 
-print bitext
+#Initialise theta probablity values
+for f in f_vocab:
+  for e in e_vocab:
+    t_prob[f,e] = 1 / float(len(e_vocab))
+
+#Initialise n which is used to store number of sentences
+n = 0
+
+'''Calculating probabilities according to the sentences in vocabulary
+stored in bitext.
+'''
+for _ in range(1):
+  for (f_sen, e_sen) in bitext:
+    n = n + 1
+    for f in f_sen:
+      if f not in f_prob:
+        f_prob[f] = {}
+        sum_t[f] = 0.0
+      total = 0.0
+      for e in e_sen:
+        if e not in e_prob:
+          e_prob[e] = 1 / float(len(e_sen))
+        else
+          e_prob[e] += 1 / float(len(e_sen))
+        if e not in f_prob[f]:
+          f_prob[f][e] = e_prob[e]
+        else:
+          f_prob[f][e] += e_prob[e]
+        total += f_prob[f][e]
+        sum_t[f] = total
+      for e in e_sen:
+        count = f_prob[f][e] / float(total)
+        if (f,e) in fe_count:
+          fe_count[(f,e)] += count 
+        else:
+          fe_count[(f,e)] = count
+        if e in e_count:
+          e_count[e] += count
+        else:
+          e_count[e] = count
+
+
+    #Calculating actual theta probabilities
+    for (f, e) in fe_count:
+      t_prob[(f,e)] += fe_count[(f,e)] / e_count[e]
+      t_prob[(f,e)] /= sum_t[f] 
+
+#print bitext
+
 
 '''
 for (n, (f, e)) in enumerate(bitext):
